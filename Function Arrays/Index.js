bb003c19d38f8e6840f90e187b07a165963b3d24
joshuatrithart <j.trithart@gmail.com>
2018-09-25 13:53:32 -0600

    update

--- Function Arrays/Index.js
+++ Function Arrays/Index.js
@@ -1,23 +1,31 @@
 
 
-// creat 10 vvariables about youself
-let joshuatrithart = {
-  Firstname: "joshua",
-  lastname: "trithart",
-  age:16,
-  ageAfraid: 34,
-  hair: "null",
-  male: true,
-}
+console.log("Using array-data-strucutre, write a function(s) to switch the content of both arrays in the second element ... the last name.")
 
-let{
-  firstname: "swiffer",
-  lastname: "wetjet",
-  age:18,
-  ageAfraid: 34,
-  hair: "null",
-  male: true,
-}
-//print ot entire object
-console.log("joshua's firstname is: " + joshuatrithart.firstname);
-console.log("swifferwetjet" +swifferwetjet.firstname);
+// Your Function Goes here
+function rewrielastnameJoshua (lastname1, lastname2) (
+    return JoshuaTrithart[1] = lastname2
+)
+
+
+  function rewritenamewetjet (lastname1, lastname2)(
+    return lastname[1] = lastname2
+)
+
+
+
+let JoshuaTrithart = [];
+let SwifferWetjet = [];
+
+// firstName, lastName, age, ageAfraid, hair, male, female
+JoshuaTrithart = ["Joshua", "Trithart", 16, 29, null, true, false];
+SwifferWetjet = ["Swiffer", "Wetjet", 16, null, true, false, true];
+
+//Your code goes here
+
+    switch (JoshuaTrithart[1], SwifferWetjet[1]);
+
+
+// Print first names only
+console.log("Last Name of JoshuaTrithart is: " + JoshuaTrithart[1]); //Should return Wetjet
+console.log("Last Name of SwifferWetjet is: " + SwifferWetjet[1]); //Should return Joshua
