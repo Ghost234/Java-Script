b460b5bf1f45479d9c95698126f795b6b53333e7
joshuatrithart <j.trithart@gmail.com>
2018-09-25 13:52:47 -0600

    update

---
+++ Index.js
@@ -0,0 +1,21 @@
+
+
+// creat 10 vvariables about youself
+let Firstname ="joshua";
+let lastname ="trithart"
+const ageAfraid = 16 ;
+let hair = "none"
+let male = true;
+
+//new code here
+//add a third  variables
+let memory = Firstname;
+Firstname = lastname;
+lastname = memory;
+
+
+
+
+// print variables to consle
+console.log (Firstname);
+console.log (lastname);
---
+++ swapping vavrables objects/Index.js
@@ -0,0 +1,23 @@
+
+
+// creat 10 vvariables about youself
+let joshuatrithart = {
+  Firstname: "joshua",
+  lastname: "trithart",
+  age:16,
+  ageAfraid: 34,
+  hair: "null",
+  male: true,
+}
+
+let{
+  firstname: "swiffer",
+  lastname: "wetjet",
+  age:18,
+  ageAfraid: 34,
+  hair: "null",
+  male: true,
+}
+//print ot entire object
+console.log("joshua's firstname is: " + joshuatrithart.firstname);
+console.log("swifferwetjet" +swifferwetjet.firstname);
