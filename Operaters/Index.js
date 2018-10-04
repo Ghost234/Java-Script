e9dcaf1258c0f6913e75689e0138781185d8ca57
joshuatrithart <j.trithart@gmail.com>
2018-10-04 13:03:48 -0600

    update

--- Operaters/Index.js
+++ Operaters/Index.js
@@ -1,1 +1,23 @@
 
+function simpleOperators (numberA, numberB){
+  let adding = numberA + numberB;
+  let subtract = numberA - numberB;
+  let mutliply = numberA * numberB;
+  let divide = numberA / numberB;
+  console.log(adding);
+  console.log(subtract);
+  console.log(mutliply);
+  console.log(divide);
+}
+//skipcount by ___ for ____ times sartingat ____
+function skipcounting (skipcount,times,startnumber){
+  for (let skipCount = 15;
+  console.log(skipCount);
+  for (let i=1; i<=5; i++){
+    skipCount *= 3;
+console.log(skipCount);
+}
+let number = [4, 7, 13, 99];
+simpleOperators (number[0], number[1]);
+simpleOperators (number[2], number[3]);
+skipcounting ();
